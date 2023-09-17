959bad5b086fad3f79409c93e95c898b0462e58b
clover <clover.hsc@gmail.com>
2023-09-17 15:03:17 +0800

    basic import tools

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
-console.log('hello world');
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
-console.log('Hi Clover');
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+//  A simple route for testing
+app.get('/', (req, res) => {
+  const resp = { result: 'success', message: 'Hello World!' }
+  res.send(resp);
+});
+
+httpServer.listen(3000, () => {
+  console.log('listening on *:3000');
+});
